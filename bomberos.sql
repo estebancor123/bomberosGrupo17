4c145ff3f3982d75a75e6a58c8af5dc0821b6d82
estebancor123 <141686271+estebancor123@users.noreply.github.com>
2023-10-16 23:20:25 -0300

    basedatos
    actualizacion base datos

--- bomberos.sql
+++ bomberos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-10-2023 a las 23:18:29
+-- Tiempo de generación: 17-10-2023 a las 04:19:26
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -31,20 +31,30 @@ USE `bomberos`;
 
 CREATE TABLE `bombero` (
   `id_bombrero` int(11) NOT NULL,
-  `nombre_ape` varchar(50) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
   `fecha_nac` date NOT NULL,
   `codBrigada` int(11) NOT NULL,
   `dni` int(8) NOT NULL,
   `celular` int(15) NOT NULL,
-  `estado` tinyint(4) NOT NULL
+  `estado` tinyint(4) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `grupo_sanguineo` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `bombero`
 --
 
-INSERT INTO `bombero` (`id_bombrero`, `nombre_ape`, `fecha_nac`, `codBrigada`, `dni`, `celular`, `estado`) VALUES
-(5, 'Esteban Cornejo', '1980-04-25', 2, 111, 12454, 1);
+INSERT INTO `bombero` (`id_bombrero`, `nombre`, `fecha_nac`, `codBrigada`, `dni`, `celular`, `estado`, `apellido`, `grupo_sanguineo`) VALUES
+(5, 'Esteban ', '1980-04-25', 2, 11111, 12454, 1, 'Cornejo', 'o-'),
+(6, 'maria ', '1990-06-11', 1, 12, 99999, 1, 'paz ', 'a+'),
+(12, 'pepe', '2023-10-18', 1, 8888, 789456123, 0, 'argento', 'a+'),
+(13, 'pepe2', '2017-10-11', 1, 6666, 123456, 1, 'argento2', 'a-'),
+(14, 'mateo', '1980-04-25', 1, 22222, 4323453, 1, 'alverto', 'a'),
+(15, 'Cordoba', '1980-04-25', 1, 7777, 5555, 1, 'Martinez', 'a+'),
+(16, 'aaaaa', '2023-10-11', 1, 111, 222, 0, 'wwwww', 'a'),
+(18, 'asdas', '2023-10-05', 1, 1112312, 123123, 1, 'asda', 'a'),
+(19, 'Cordoba', '1980-04-25', 1, 7777, 5555, 1, 'Martinez', 'a+');
 
 -- --------------------------------------------------------
 
@@ -66,9 +76,10 @@ CREATE TABLE `brigada` (
 --
 
 INSERT INTO `brigada` (`codBrigada`, `nombre_br`, `especialidad`, `libre`, `nro_cuartel`, `estado`) VALUES
-(1, 'eeee', 'fuego', 1, 2, 1),
-(2, 'uuuu', 'mofuego', 1, 3, 1),
-(4, 'qqqq', 'masfuego', 1, 5, 1);
+(1, 'sin asignar', 'sin asignar', 1, 2, 1),
+(2, 'delta', 'rescates en montaña', 0, 3, 1),
+(4, 'start', 'socorrer inundaciones', 1, 5, 1),
+(5, 'netbean', 'operativos de prevención', 1, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -83,17 +94,20 @@ CREATE TABLE `cuartel` (
   `coord_X` int(11) NOT NULL,
   `coord_Y` int(11) NOT NULL,
   `telefono` int(15) NOT NULL,
-  `correo` varchar(30) NOT NULL
+  `correo` varchar(30) NOT NULL,
+  `estado` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `cuartel`
 --
 
-INSERT INTO `cuartel` (`cod_Cuartel`, `nombre_cuartel`, `direccion`, `coord_X`, `coord_Y`, `telefono`, `correo`) VALUES
-(2, 'cuartelNumero1', 'cordoba 7444', 1, 2, 4555005, 'correo'),
-(3, 'cuartelNumero2', 'mendoza 5555', 2, 3, 9999995, 'correo2'),
-(5, 'cuartelNumero3', 'San martin 6666', 3, 4, 7777777, 'correo3');
+INSERT INTO `cuartel` (`cod_Cuartel`, `nombre_cuartel`, `direccion`, `coord_X`, `coord_Y`, `telefono`, `correo`, `estado`) VALUES
+(2, 'cuartelNumero1', 'cordoba 7444', 23, 2, 4555005, 'correo', 1),
+(3, 'cuartelNumero2', 'mendoza 5555', 2, 3, 9999995, 'correo2', 1),
+(5, 'cuartelNumero3', 'San martin 6666', 3, 4, 7777777, 'correo3', 1),
+(7, 'cuartelNumero4', '9  de julio 1019', 5, 9, 888888, 'correo4', 1),
+(9, 'Cocacola', 'por ahi', 55, 44, 4550055, 'radiotaxi', 1);
 
 -- --------------------------------------------------------
 
@@ -118,7 +132,12 @@ CREATE TABLE `siniestro` (
 --
 
 INSERT INTO `siniestro` (`codigo`, `tipo`, `fecha_siniestro`, `coord_X`, `coord_Y`, `detalle`, `fecha_resol`, `puntuacion`, `codBrigada`) VALUES
-(1, ') incendios en vivie', '1980-04-20', 0, 0, 'se que el rancho', '2023-10-05', 8, 4);
+(1, 'incendios', '2023-10-15', 12, 15, 'se que el rancho', '2023-10-16', 8, 4),
+(2, 'incendios', '2023-10-16', 14, 12, 'incedio de casa 2 pisos', '2023-10-16', 6, 5),
+(4, 'rescates en montaña', '1980-04-20', 0, 0, 'se que el rancho', '2023-10-05', 9, 4),
+(5, 'rescates en montaña', '2023-10-18', 15, 12, 'en la altura', '0000-00-00', 0, 2),
+(6, 'incendios', '2023-10-16', 15, 32, 'fuego mucho', '0000-00-00', 0, 1),
+(7, 'incendios', '2023-10-16', 12, 12, 'asdasd', '0000-00-00', 0, 1);
 
 --
 -- Índices para tablas volcadas
@@ -159,25 +178,25 @@ ALTER TABLE `siniestro`
 -- AUTO_INCREMENT de la tabla `bombero`
 --
 ALTER TABLE `bombero`
-  MODIFY `id_bombrero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_bombrero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `brigada`
 --
 ALTER TABLE `brigada`
-  MODIFY `codBrigada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `codBrigada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `cuartel`
 --
 ALTER TABLE `cuartel`
-  MODIFY `cod_Cuartel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `cod_Cuartel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `siniestro`
 --
 ALTER TABLE `siniestro`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
